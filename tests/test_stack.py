66ca67caebc581a84ed6f8b1a16d2eef0df6290c
akasmall <aka_sm@ro.ru>
2023-11-18 18:29:15 +0300

    m02_p04_lesson_05 part 2

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_stack():
     stack = []
     # Добавляем два элемента в стек и затем извлекаем их
@@ -34,3 +37,12 @@ def test_emptiness():
 
     stack.pop()
     assert not stack
+
+
+def test_pop_with_empty_stack():
+    stack = []
+    # проверить что вызывается конкретное исключение можно с помощью
+    # конструкции with pytest.raises()
+    # если внутри блока вызовется исключение, то тест будет пройден
+    with pytest.raises(IndexError):
+        stack.pop()
